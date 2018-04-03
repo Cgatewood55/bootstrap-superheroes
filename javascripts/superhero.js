002c208215c3b04246b3a4dc8a3c3815a542933b
Chris Gatewood <c.gatewood55@gmail.com>
2018-04-03 18:43:30 -0500

    wip

--- javascripts/superhero.js
+++ javascripts/superhero.js
@@ -23,9 +23,18 @@ function buildDomDtring(superheroes)
 {
     let domString = "";
     superheroes.forEach(superhero => {
-        domString += `<div class=cards>`;
-        domString += `<h1> ${superhero.name}</h1>`;
-        domString += `</div>`
+        domString += `<div class="col-sm-3">`;
+        domString += `<div class="panel panel-default">        `;
+        domString += `<div class ="panel-heading">`;
+        domString += `<h3 class="panel-title">${superhero.name}</h3>`;
+        domString += `</div>`;
+        domString += `<div class = "panel-body">`;
+        domString += `<img class="charImage" src"${superhero.image}">`;
+        domString += `<p class="charDescription">${superhero.description}</p>`;
+        domString += `Panel content`;
+        domString += `</div>`;
+        domString += `</div>`;
+        domString += `</div>`;
 
         
     });
@@ -38,7 +47,7 @@ function startApplication()
     let myrequest = new XMLHttpRequest();
     myrequest.addEventListener("load",executeThisCodeAfterFileLoaded);
     myrequest.addEventListener("error",executeThisCodeIfXHRFails);
-    myrequest.open("GET","db/superheroes.json");
+    myrequest.open("GET","/db/superheroes.json");
     myrequest.send(myrequest);
 }
 startApplication();
