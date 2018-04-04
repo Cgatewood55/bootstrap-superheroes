1c2b7bd00d0a9773484aa5e2b25be18e8310c644
Chris Gatewood <c.gatewood55@gmail.com>
2018-04-03 19:59:29 -0500

    dropdown

--- javascripts/jobs.js
+++ javascripts/jobs.js
@@ -1,1 +1,44 @@
-console.log('hello')
+function printToDom(domString,divId)
+{
+    document.getElementById(divId).innerHTML= domString;
+}
+
+
+
+
+function executeThisCodeAfterFileLoaded()
+{
+    const data = JSON.parse(this.responseText);
+    buildDomDtring(data.superheroes);
+
+}
+
+function executeThisCodeIfXHRFails() 
+{
+    console.log("something went wrong")
+
+}
+
+function buildDomDtring(superheroes)
+{
+    let domString = "";
+    superheroes.forEach(superhero => {
+        domString += `<li>`;
+        domString +=    `<a href="#">${superhero.name}</a>`;
+        domString += `<li>`;
+
+        
+    });
+    printToDom(domString,"awesome-dropdown");
+
+};
+
+function startApplication()
+{
+    let myrequest = new XMLHttpRequest();
+    myrequest.addEventListener("load",executeThisCodeAfterFileLoaded);
+    myrequest.addEventListener("error",executeThisCodeIfXHRFails);
+    myrequest.open("GET","/db/superheroes.json");
+    myrequest.send(myrequest);
+}
+startApplication();
